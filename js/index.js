538e778fb2f5ae59c806cf7fa8d79fa394c796c8
Your Name <your.email@example.com>
2026-05-19 19:11:13 -0400

    petit ajustements

--- js/index.js
+++ js/index.js
@@ -21,14 +21,10 @@ async function chargerCours() {
                     <h3 class="carte-nom">${c.titre}</h3>
                     <p><strong>Niveau :</strong> ${c.niveau}</p>
                     <p><strong>Capacité :</strong> ${c.capacite_max}</p>
-<<<<<<< HEAD
                     <p><strong>Instructeur :</strong> ${c.id_instructeur}</p>
                     <a href="detail-cours.html?id=${c.id_cours}" class="btn-ajouter" style="display:inline-block; margin-top:15px; text-decoration:none; text-align:center;">
                         Détails
                     </a>
-=======
-                    <p><strong>Instructeur :</strong> ${c.nom_instructeur}</p>
->>>>>>> f9cd86a1dd1c04fad0a729d91f1477abfb215061
                 </div>
             `;
 
